2b167309dbbced95446e0b9c27fccc3234ad4619
Jat <1356020049@qq.com>
2022-07-17 15:56:51 +0800

    解决逻辑错误导致重新选择地点闪退问题和部分方法落后问题。

--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
@@ -25,28 +25,16 @@ class PlaceFragment : Fragment() {
     private var _binding: FragmentPlaceBinding? = null
     private val binding get() = _binding!!
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentPlaceBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    @Deprecated("Deprecated in Java")
-    @SuppressLint("NotifyDataSetChanged")
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
         /*在这里进行判断当前是否已经有存储的城市数据，有就直接跳转
-        * 加多一层判断：只有当PlaceFragment嵌入MainActivity时才会直接跳转，
-        * 解决了在WeatherActivity更换地点无限跳转的问题*/
+       * 加多一层判断：只有当PlaceFragment嵌入MainActivity时才会直接跳转，
+       * 解决了在WeatherActivity更换地点无限跳转的问题*/
         if (activity is MainActivity && viewModel.isPlaceSaved()) {
             val place = viewModel.getSavedPlace()
             val intent = Intent(context, WeatherActivity::class.java).apply {
@@ -56,7 +44,7 @@ class PlaceFragment : Fragment() {
             }
             startActivity(intent)
             activity?.finish()
-            return
+            return binding.root
         }
 
         val layoutManager = LinearLayoutManager(activity)
@@ -90,6 +78,65 @@ class PlaceFragment : Fragment() {
                 result.exceptionOrNull()?.printStackTrace()
             }
         })
+        return binding.root
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+//    @Deprecated("Deprecated in Java")
+//    @SuppressLint("NotifyDataSetChanged")
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//
+//        /*在这里进行判断当前是否已经有存储的城市数据，有就直接跳转
+//        * 加多一层判断：只有当PlaceFragment嵌入MainActivity时才会直接跳转，
+//        * 解决了在WeatherActivity更换地点无限跳转的问题*/
+//        if (activity is MainActivity && viewModel.isPlaceSaved()) {
+//            val place = viewModel.getSavedPlace()
+//            val intent = Intent(context, WeatherActivity::class.java).apply {
+//                putExtra("location_lng", place.location.lng)
+//                putExtra("location_lat", place.location.lat)
+//                putExtra("place_name", place.name)
+//            }
+//            startActivity(intent)
+//            activity?.finish()
+//            return
+//        }
+//
+//        val layoutManager = LinearLayoutManager(activity)
+//        binding.recyclerView.layoutManager = layoutManager
+//        adapter = PlaceAdapter(this, viewModel.placeList)
+//        binding.recyclerView.adapter = adapter
+//
+//        /*给搜索栏添加文本变化监听器*/
+//        binding.searchPlaceEdit.addTextChangedListener { editable ->
+//            val content = editable.toString()
+//            if (content.isNotEmpty()) {
+//                viewModel.searchPlaces(content)
+//            } else {
+//                binding.recyclerView.visibility = View.GONE
+//                binding.bgImageView.visibility = View.VISIBLE
+//                viewModel.placeList.clear()
+//                adapter.notifyDataSetChanged()
+//            }
+//        }
+//
+//        viewModel.placeLiveData.observe(viewLifecycleOwner, Observer { result ->
+//            val places = result.getOrNull()
+//            if (places != null) {
+//                binding.recyclerView.visibility = View.VISIBLE
+//                binding.bgImageView.visibility = View.GONE
+//                viewModel.placeList.clear()
+//                viewModel.placeList.addAll(places)
+//                adapter.notifyDataSetChanged()
+//            } else {
+//                Toast.makeText(activity, "未能查询到任何地点", Toast.LENGTH_SHORT).show()
+//                result.exceptionOrNull()?.printStackTrace()
+//            }
+//        })
+//    }
 
 }
